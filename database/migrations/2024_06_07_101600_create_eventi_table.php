8cbbe56050a1eb8458221166139c75ef6f14003e
Alessandro9827 <ale.moramarco27@gmail.com>
2024-06-07 16:43:08 +0200

    fixed problems

--- database/migrations/2024_06_07_101600_create_eventi_table.php
+++ database/migrations/2024_06_07_101600_create_eventi_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('eventi', function (Blueprint $table) {
+        Schema::create('eventos', function (Blueprint $table) {
             $table->id();
             $table->string('nome_evento');
             $table->date('data_evento');
